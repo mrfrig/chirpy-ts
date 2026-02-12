718c14eca38c9737abe7af6f2abc929918abb65e
Franklin Isalguez <franklin@neosmail.com>
2026-02-12 12:14:26 -0400

    4-3 Use express json middleware

--- src/api/validateChirp.ts
+++ src/api/validateChirp.ts
@@ -13,37 +13,26 @@ type ResponseData = {
 };
 
 export function handlerValidateChirp(req: Request, res: Response) {
-  let body = ""; 
+  const params: RequestData = req.body;
 
-  req.on("data", (chunk) => {
-    body += chunk;
-  });
-
-  req.on("end", () => {
-    let msg = "";
-    try {
-      const parsedBody = JSON.parse(body) as RequestData;
-      if( !("body" in parsedBody)) throw new Error();
-      msg = parsedBody.body;
-    } catch (error) {
-      const respBody: ResponseError = {
+  if( !("body" in params)) {
+    const respBody: ResponseError = {
         error: "Invalid JSON"
-      };
-      res.status(400).send(JSON.stringify(respBody));
-      return;
-    }
-
-    if (msg.length > 140) {
-      const respBody: ResponseError = {
-        error: "Chirp is too long"
-      };
-      res.status(400).send(JSON.stringify(respBody));
-      return;
-    }
-
-    const respBody: ResponseData = {
-        valid: true
     };
-    res.status(200).send(JSON.stringify(respBody));
-  });
+    res.status(400).send(JSON.stringify(respBody));
+    return;
+  }
+
+  if (params.body.length > 140) {
+    const respBody: ResponseError = {
+      error: "Chirp is too long"
+    };
+    res.status(400).send(JSON.stringify(respBody));
+    return;
+  }
+
+  const respBody: ResponseData = {
+      valid: true
+  };
+  res.status(200).send(JSON.stringify(respBody));
 }
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { handlerValidateChirp } from "./api/validateChirp.js";
 const app = express();
 const PORT = 8080;
 
+app.use(express.json());
 app.use(middlewareLogResponses);
 
 app.use("/app",middlewareMetricsInc, express.static("./src/app"));
